6cfca9ab9b0ce3e9f021579422d4324c1365b009
Baptiste Ruiz <bruiz@genossh.genouest.org>
2024-03-18 17:55:09 +0100

    Reorganized core meta output

--- Post-processing/export_core_and_meta.py
+++ Post-processing/export_core_and_meta.py
@@ -5,6 +5,7 @@ from Bio.ExPASy import Enzyme
 from goatools import obo_parser
 import requests
 import os
+import ast
 
 import argparse
 
@@ -31,64 +32,30 @@ pipeline_path = args.pipeline_path
 
 ###FUNCTION
 
-def add_reaction_names(dataframe):
+def extract_core_associates(dataframe, core_list):
     '''
-    This function queries the OBO and ExPASy data banks to get the names of the IDs
+    This function automatically extracts the core-significant from the lists of associated variables
     '''
-    
-    #bar = IncrementalBar('Recovering Names', max = len(dataframe.iloc[:,1]))
 
-    data_folder = pipeline_path + '/Post-processing/data'
-
-    # Check if we have the ./data directory already
-    if(not os.path.isfile(data_folder)):
-        # Emulate mkdir -p (no error if folder exists)
-        try:
-            os.mkdir(data_folder)
-        except OSError as e:
-            if(e.errno != 17):
-                raise e
+    if 'Linked_Reactions' in dataframe.columns:
+        col_ref = 'Linked_Reactions'
+        new_col = 'Significant_linked_Reactions'
     else:
-        raise Exception('Data path (' + data_folder + ') exists as a file. '
-                    'Please rename, remove or change the desired location of the data path.')
+        col_ref = 'Linked_OTUs'
+        new_col = 'Significant_linked_OTUs'
+
+    signif_vars = []
+    for vars_assoc in dataframe[col_ref].values:
+        vars_list = []
+        for var in ast.literal_eval(vars_assoc):
+            if var in core_list:
+                vars_list.append(var)
+        signif_vars.append(vars_list)
     
-    #Check if we have enzyme file, if not download it
-    if not(os.path.isfile(data_folder + '/enzyme.dat')):
-        url = "https://ftp.expasy.org/databases/enzyme/enzyme.dat"
-        r = requests.get(url, allow_redirects=True)
-        open(data_folder + '/enzyme.dat', 'wb').write(r.content)
-
-    #Same with GO file
-    if(not os.path.isfile(data_folder+'/go-basic.obo')):
-        go_obo_url = 'http://purl.obolibrary.org/obo/go/go-basic.obo'
-        r = requests.get(go_obo_url, allow_redirects=True)
-        open(data_folder + '/go-basic.obo', 'wb').write(r.content)
-
-    
-    go = obo_parser.GODag(data_folder+'/go-basic.obo')
-    
-    reaction_names = []
-
-    for id in dataframe.iloc[:,0]:
-        #print("id=", id)
-        reaction_names.append("Not Found")
-
-        if "GO" in id:
-            if id in go:
-                go_term = go[id]
-                reaction_names[-1] = go_term.name
-        else:
-            handle = open(data_folder + '/enzyme.dat')
-            records = Enzyme.parse(handle)
-            de_found = next((item["DE"] for item in records if item["ID"] == id), "Not Found")
-            reaction_names[-1] = de_found
-            #print("record_id = ", record["DE"])
-        
-        #bar.next()
-    #print(len(reaction_names))
-    dataframe["Names"] = reaction_names
-    #dataframe.columns = ["Average_importance","ID","Name"]
+    dataframe[new_col] = signif_vars
     return dataframe
+
+    
 
 
 ###GATHER ALL SIGNIFICANT LISTS
@@ -96,6 +63,9 @@ def add_reaction_names(dataframe):
 link_to_test_folder = pipeline_path+'/Meta_Outputs/'+args.data_ref
 iterations=int(args.nb_iterations)
 dict_lists = {}
+median_perfs_otus = defaultdict(list)
+median_perfs_sofas = defaultdict(list)
+
 for i in range(iterations):
     iter_folder = args.data_ref+'_'+str(i+1)
     for run_nb in os.listdir(link_to_test_folder+'/'+iter_folder+'/Selection_outputs'):
@@ -104,7 +74,6 @@ for i in range(iterations):
         
         signif_list_sofa = score_db.truncate(after = score_db.loc[score_db['ID']=='CUTOFF'].index.values[0]-1)['ID'].values
         signif_list_otu = otu_db.truncate(after = otu_db.loc[otu_db['ID']=='CUTOFF'].index.values[0]-1)['ID'].values
-        print(signif_list_otu)
 
         if 'OTU' not in dict_lists.keys():
             dict_lists['OTU'] = {}
@@ -120,12 +89,36 @@ for i in range(iterations):
         dict_lists['OTU'][run_nb][i+1] = signif_list_otu
         dict_lists['SoFA'][run_nb][i+1] = signif_list_sofa
 
+    for run_nb in os.listdir(link_to_test_folder+'/'+iter_folder+'/Classification_performances'):
+        otu_classif_perfs = pd.read_csv(link_to_test_folder+'/'+iter_folder+'/Classification_performances/'+run_nb+'/OTU_classif_perfs.csv')['Test performance'].values
+        sofa_classif_perfs = pd.read_csv(link_to_test_folder+'/'+iter_folder+'/Classification_performances/'+run_nb+'/SoFA_classif_perfs.csv')['Test performance'].values
+
+        median_perfs_otus[run_nb].append(np.median(otu_classif_perfs))
+        median_perfs_sofas[run_nb].append(np.median(sofa_classif_perfs))
+
+
+for i in median_perfs_sofas.keys():
+    median_perfs_sofas[i] = np.mean(median_perfs_sofas[i])
+
+for i in median_perfs_otus.keys():
+    median_perfs_otus[i] = np.mean(median_perfs_otus[i])
+
+
+print(median_perfs_otus)
+max_ind_sofas = max(median_perfs_sofas, key=median_perfs_sofas.get)
+max_ind_otus = max(median_perfs_otus, key=median_perfs_otus.get)
 
 ###CREATE CORE AND META LISTS
 
-dict_core_meta = defaultdict(defaultdict)
 if not os.path.exists(link_to_test_folder+'/core_and_meta_all_runs'):
     os.makedirs(link_to_test_folder+'/core_and_meta_all_runs')
+if not os.path.exists(link_to_test_folder+'/core_and_meta_all_runs/All_iterations'):
+    os.makedirs(link_to_test_folder+'/core_and_meta_all_runs/All_iterations')
+if not os.path.exists(link_to_test_folder+'/core_and_meta_all_runs/Best_iteration'):
+    os.makedirs(link_to_test_folder+'/core_and_meta_all_runs/Best_iteration')
+
+
+core_meta_dfs_archive = {'OTU':{'core':defaultdict(defaultdict), 'meta':defaultdict(defaultdict)}, 'SoFA': {'core':defaultdict(defaultdict), 'meta':defaultdict(defaultdict)}}
 
 for profile in ['OTU','SoFA']:
     for run in dict_lists[profile].keys():
@@ -140,14 +133,105 @@ for profile in ['OTU','SoFA']:
             else:
                 core_meta['meta']['ID'].append(annot)
                 core_meta['meta']['Count'].append(full_list.count(annot))
-        
+
         
         core = pd.DataFrame(core_meta['core'], columns=['ID'])
         meta = pd.DataFrame(core_meta['meta']).sort_values(by='Count', ascending=False)
 
-        if profile =='SoFA':
-            core = add_reaction_names(core)
-            meta = add_reaction_names(meta)
+        # if profile =='SoFA':
+        #     core = add_reaction_names(core)
+        #     meta = add_reaction_names(meta)
 
-        core.to_csv(link_to_test_folder +'/core_and_meta_all_runs/core_'+profile+'_'+dataset_name+'_iteration_'+run+'.csv', index=0)
-        meta.to_csv(link_to_test_folder +'/core_and_meta_all_runs/meta_'+profile+'_'+dataset_name+'_iteration_'+run+'.csv', index=0)
+        
+        core_meta_dfs_archive[profile]['core'][run] = core
+        core_meta_dfs_archive[profile]['meta'][run] = meta
+        
+
+##Getting the reference info for OTUs and annotations
+otu_db = pd.read_csv(link_to_test_folder+'/'+args.data_ref+'_1/Selection_outputs/run_1/OTU_'+dataset_name+'.csv')[['Average_importance','ID','Linked_Reactions','Family']]
+sofa_db = pd.read_csv(link_to_test_folder+'/'+args.data_ref+'_1/Selection_outputs/run_1/scores_'+dataset_name+'.csv')[['Average_importance','ID','Name','Linked_OTUs','Family']]
+
+otu_db = otu_db[otu_db['ID']!='CUTOFF']
+sofa_db = sofa_db[sofa_db['ID']!='CUTOFF']
+
+##Adding OTU taxonomies
+otu_names_ref = pd.read_csv(link_to_test_folder+'/'+args.data_ref+'_1/SoFAs/OTU_name_table_ref.tsv', sep='\t')
+otu_taxonomies = []
+
+for otu in otu_db['ID'].values:
+    otu_taxo = otu_names_ref[otu_names_ref['observation_name']==otu]['taxonomic_affiliation'].values
+    otu_taxonomies.append(otu_taxo)
+
+otu_db['Taxonomy'] = otu_taxonomies
+
+##Adding each variable's presence in each profile
+
+labels = np.unique(pd.read_csv(pipeline_path+'/Inputs/Label_'+dataset_name+'.csv', header=None).values)
+label_avg_counts ={'OTU':{}, 'SoFA':{}}
+
+if 'tf_igm' in args.data_ref:
+    data_call = dataset_name + '_tfigm'
+elif 'scaled' in args.data_ref:
+    data_call =args.data_ref
+else:
+    data_call = dataset_name
+
+for lab in labels:
+    otu_avg_label = pd.read_csv(pipeline_path+'/Post-processing/outputs/'+data_call+'_avg_difference_count_'+str(lab)+'.csv', index_col=0)['average']
+    sofa_avg_label = pd.read_csv(pipeline_path+'/Post-processing/outputs/'+data_call+'_avg_difference_score_'+str(lab)+'.csv', index_col=0)['average']
+
+    otu_db['average score in '+str(lab)] = otu_avg_label.loc[list(otu_db['ID'].values)].values
+    sofa_db['average score in '+str(lab)] = sofa_avg_label.loc[list(sofa_db['ID'].values)].values
+    
+
+
+##Formatting the Core and Meta output files
+for run in dict_lists[profile].keys():
+    core_otu_df = core_meta_dfs_archive['OTU']['core'][run]
+    meta_otu_df = core_meta_dfs_archive['OTU']['meta'][run]
+    core_sofa_df = core_meta_dfs_archive['SoFA']['core'][run]
+    meta_sofa_df = core_meta_dfs_archive['SoFA']['meta'][run]
+
+    core_otu_df_plus_info = otu_db[otu_db['ID'].isin(core_otu_df['ID'])]
+    core_sofa_df_plus_info = sofa_db[sofa_db['ID'].isin(core_sofa_df['ID'])]
+
+    meta_otu_df_plus_info = otu_db[otu_db['ID'].isin(meta_otu_df['ID'])]
+    meta_sofa_df_plus_info = sofa_db[sofa_db['ID'].isin(meta_sofa_df['ID'])]
+    meta_otu_df_plus_info['Count'] = meta_otu_df['Count']
+    meta_sofa_df_plus_info['Count'] = meta_sofa_df['Count']
+
+
+    core_otu_df_plus_info = extract_core_associates(core_otu_df_plus_info, core_sofa_df['ID'].values)
+    meta_otu_df_plus_info = extract_core_associates(meta_otu_df_plus_info, core_sofa_df['ID'].values)
+    
+    core_sofa_df_plus_info = extract_core_associates(core_sofa_df_plus_info, core_otu_df['ID'].values)
+    meta_sofa_df_plus_info = extract_core_associates(meta_sofa_df_plus_info, core_otu_df['ID'].values)
+
+    core_sofa_df_plus_info = core_sofa_df_plus_info[['Average_importance','ID','Name','Linked_OTUs','Significant_linked_OTUs','Family']+['average score in '+str(lab) for lab in labels]]
+    meta_sofa_df_plus_info = meta_sofa_df_plus_info[['ID','Count','Average_importance','Name','Linked_OTUs','Significant_linked_OTUs','Family']+['average score in '+str(lab) for lab in labels]]
+    core_otu_df_plus_info = core_otu_df_plus_info[['Average_importance','ID','Taxonomy','Linked_Reactions','Significant_linked_Reactions','Family']+['average score in '+str(lab) for lab in labels]]
+    meta_otu_df_plus_info = meta_otu_df_plus_info[['ID','Count','Average_importance','Taxonomy','Linked_Reactions','Significant_linked_Reactions','Family']+['average score in '+str(lab) for lab in labels]]
+
+    core_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/core_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    meta_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/meta_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    core_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/core_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    meta_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/meta_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+
+    if run == max_ind_sofas:
+        core_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/core_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+        meta_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/meta_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+
+    if run == max_ind_otus:
+        core_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/core_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+        meta_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/meta_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+
+    core_meta_dfs_archive['OTU']['core'][run] = core_otu_df_plus_info
+    core_meta_dfs_archive['OTU']['meta'][run] = meta_otu_df_plus_info
+    core_meta_dfs_archive['SoFA']['core'][run] = core_sofa_df_plus_info
+    core_meta_dfs_archive['SoFA']['meta'][run] = meta_sofa_df_plus_info
+
+    
+
+
+
+
