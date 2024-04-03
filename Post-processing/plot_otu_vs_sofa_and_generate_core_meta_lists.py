f5dc123e9931e35e66c646c04d140f4928b60388
Baptiste Ruiz <bruiz@cl1n038.genouest.org>
2024-04-03 12:03:22 +0200

    Combining perf plots and core meta exports

--- Post-processing/plot_otu_vs_sofa_and_generate_core_meta_lists.py
+++ Post-processing/plot_otu_vs_sofa_and_generate_core_meta_lists.py
@@ -1,11 +1,11 @@
+import seaborn as sns
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from collections import defaultdict
-from Bio.ExPASy import Enzyme
-from goatools import obo_parser
-import requests
 import os
 import ast
+
 
 import argparse
 
@@ -29,6 +29,7 @@ args = parser.parse_args()
 dataset_name = args.dataset_name
 
 pipeline_path = args.pipeline_path
+
 
 ###FUNCTION
 
@@ -55,22 +56,41 @@ def extract_core_associates(dataframe, core_list):
     dataframe[new_col] = signif_vars
     return dataframe
 
-    
+
+###COMPILING INFO ABOUT ALL RUNS AND GATHERING ALL SIGNIFICANT LISTS
+
+path_save = pipeline_path + '/Meta_Outputs/'+args.data_ref
+
+median_perf_values_dict = {}
+
+median_otu_test_perfs = defaultdict(list)
+median_sofa_test_perfs = defaultdict(list)
 
 
-###GATHER ALL SIGNIFICANT LISTS
-
-link_to_test_folder = pipeline_path+'/Meta_Outputs/'+args.data_ref
-reps=int(args.nb_repeats)
 dict_lists = {}
-median_perfs_otus = defaultdict(list)
-median_perfs_sofas = defaultdict(list)
 
-for i in range(reps):
-    iter_folder = args.data_ref+'_'+str(i+1)
-    for run_nb in os.listdir(link_to_test_folder+'/'+iter_folder+'/Selection_outputs'):
-        otu_db = pd.read_csv(link_to_test_folder+'/'+iter_folder+'/Selection_outputs/'+run_nb+'/OTU_'+dataset_name+'.csv')
-        score_db = pd.read_csv(link_to_test_folder+'/'+iter_folder+'/Selection_outputs/'+run_nb+'/scores_'+dataset_name+'.csv')
+for run_nb in range(1, int(args.nb_repeats) + 1):
+     
+    path_ref = path_save +'/'+args.data_ref+'_'+str(run_nb)
+
+    for i in range(int(args.nb_iterations)):
+    #Gather test classification performances
+
+        local_path = path_ref + '/Classification_performances/run_'+str(i+1)+'/SoFA_classif_perfs.csv'   
+
+        dataframe_sofa = pd.read_csv(local_path, sep = ',')
+        median_sofa_test_perfs[i].append(np.median(list(dataframe_sofa['Test performance'].values)))
+
+        local_path_otu = path_ref + '/Classification_performances/run_'+str(i+1)+'/OTU_classif_perfs.csv'
+        dataframe_otu = pd.read_csv(local_path_otu, sep = ',')
+        median_otu_test_perfs[i].append(np.median(list(dataframe_otu['Test performance'].values)))
+    
+    median_perf_values_dict[dataset_name] = [median_sofa_test_perfs, median_otu_test_perfs]
+
+    for run_folder in os.listdir(path_ref+'/Selection_outputs'):
+    #Gather the lists of selected variables per run and iteration
+        otu_db = pd.read_csv(path_ref+'/Selection_outputs/'+run_folder+'/OTU_'+dataset_name+'.csv')
+        score_db = pd.read_csv(path_ref+'/Selection_outputs/'+run_folder+'/scores_'+dataset_name+'.csv')
         
         signif_list_sofa = score_db.truncate(after = score_db.loc[score_db['ID']=='CUTOFF'].index.values[0]-1)['ID'].values
         signif_list_otu = otu_db.truncate(after = otu_db.loc[otu_db['ID']=='CUTOFF'].index.values[0]-1)['ID'].values
@@ -81,39 +101,95 @@ for i in range(reps):
         if 'SoFA' not in dict_lists.keys():
             dict_lists['SoFA'] = {}
         
-        if run_nb not in dict_lists['OTU'].keys():
-            dict_lists['OTU'][run_nb]=defaultdict(list)
-        if run_nb not in dict_lists['SoFA'].keys():
-            dict_lists['SoFA'][run_nb]=defaultdict(list)
+        if run_folder not in dict_lists['OTU'].keys():
+            dict_lists['OTU'][run_folder]=defaultdict(list)
+        if run_folder not in dict_lists['SoFA'].keys():
+            dict_lists['SoFA'][run_folder]=defaultdict(list)
         
-        dict_lists['OTU'][run_nb][i+1] = signif_list_otu
-        dict_lists['SoFA'][run_nb][i+1] = signif_list_sofa
-
-    for run_nb in os.listdir(link_to_test_folder+'/'+iter_folder+'/Classification_performances'):
-        otu_classif_perfs = pd.read_csv(link_to_test_folder+'/'+iter_folder+'/Classification_performances/'+run_nb+'/OTU_classif_perfs.csv')['Test performance'].values
-        sofa_classif_perfs = pd.read_csv(link_to_test_folder+'/'+iter_folder+'/Classification_performances/'+run_nb+'/SoFA_classif_perfs.csv')['Test performance'].values
-
-        median_perfs_otus[run_nb].append(np.median(otu_classif_perfs))
-        median_perfs_sofas[run_nb].append(np.median(sofa_classif_perfs))
+        dict_lists['OTU'][run_folder][i+1] = signif_list_otu
+        dict_lists['SoFA'][run_folder][i+1] = signif_list_sofa
 
 
-for i in median_perfs_sofas.keys():
-    median_perfs_sofas[i] = np.mean(median_perfs_sofas[i])
+###PLOTTING MEDIAN PERFORMANCES
 
-for i in median_perfs_otus.keys():
-    median_perfs_otus[i] = np.mean(median_perfs_otus[i])
+ticks = ['Original Data', 'First Selection', 'Second Selection', 'Third Selection', 'Fourth Selection']
 
-max_ind_sofas = max(median_perfs_sofas, key=median_perfs_sofas.get)
-max_ind_otus = max(median_perfs_otus, key=median_perfs_otus.get)
+sns.set_theme()
+sns.set_context('paper')
+sns.set_style('whitegrid')
+
+fig,ax = plt.subplots(1,constrained_layout=True, figsize=(10,10))
+
+best_runs = []
+max_vals = []
+for i, disease_name in enumerate(median_perf_values_dict.keys()):
+    
+    
+    sofa_test_perfs = median_perf_values_dict[disease_name][0]
+
+    otu_test_perfs = median_perf_values_dict[disease_name][1]
+    
+    median_sofas_perfs_tfigm = [np.mean(sofa_test_perfs[i]) for i in sofa_test_perfs.keys()]
+    median_otu_perfs = [np.mean(otu_test_perfs[i]) for i in otu_test_perfs.keys()]
+    
+
+    max_ind_sofas = median_sofas_perfs_tfigm.index(max(median_sofas_perfs_tfigm))
+    max_ind_otus = median_otu_perfs.index(max(median_otu_perfs))
+
+
+    best_runs+=[max_ind_sofas] + [max_ind_otus]
+
+    max_vals.append(max(sofa_test_perfs[max_ind_sofas]))
+    max_vals.append(max(otu_test_perfs[max_ind_otus]))
+
+    dataframe = pd.DataFrame.from_dict({'AUC score': sofa_test_perfs[max_ind_sofas]+otu_test_perfs[max_ind_otus]})
+    dataframe['Profile']=['SoFAs' for i in range(len(sofa_test_perfs[max_ind_sofas]))]+['OTUs' for i in range(len(otu_test_perfs[max_ind_otus]))]
+    dataframe['Disease']=[disease_name.split('_')[1] for i in range(len(sofa_test_perfs[max_ind_sofas])+len(otu_test_perfs[max_ind_otus]))]
+    dataframe['Best run']=[max_ind_sofas for i in range(len(sofa_test_perfs[max_ind_sofas]))] + [max_ind_otus for i in range(len(otu_test_perfs[max_ind_otus]))]
+    dataframe['Median']=[median_sofas_perfs_tfigm[max_ind_sofas] for i in range(len(sofa_test_perfs[max_ind_sofas]))] + [median_otu_perfs[max_ind_otus] for i in range(len(otu_test_perfs[max_ind_otus]))]
+    
+    if i == 0:
+        df_cumul = dataframe
+    else:
+        df_cumul = pd.concat([df_cumul, dataframe])
+
+medians = sns.boxplot(
+            y='Disease',
+            x='AUC score',
+            hue = 'Profile',
+            data=df_cumul,
+            ax=ax,
+            palette = 'pastel',
+            fliersize = 0)
+sns.stripplot(y='Disease', x='AUC score', hue = 'Profile', data=df_cumul, jitter = True, dodge=True, ax=ax, palette = 'dark')
+
+
+max_val = max(max_vals)
+plt.text(max_val + 0.01, -0.5, 'Optimal number of selections', size=15, fontweight = 'bold')
+for xi,yi,tx, col in zip([max_val + 0.03 for i in max_vals], (np.arange(len(dataframe['Median'].values))/2)-0.2, best_runs, ['blue','darkorange']*6):
+     plt.text( xi, yi,tx, size=18, fontweight = 'bold', c=col)
+# plt.text(1.01, -0.8, 'Optimal selection', size=10, fontweight = 'bold')
+
+handles, labels = ax.get_legend_handles_labels()
+l = plt.legend(handles[2:], labels[2:], loc='upper left', borderaxespad=0., fontsize = 15)
+
+plt.yticks(size=15)
+ax.set_ylabel('Disease',size=20)
+
+plt.xticks(size=15)
+ax.set_xlabel('Median AUC',size=20)
+
+plt.savefig(path_save +'/median_OTU_vs_SoFA_(best_vs_best).png', dpi=300)
+
 
 ###CREATE CORE AND META LISTS
 
-if not os.path.exists(link_to_test_folder+'/core_and_meta_all_runs'):
-    os.makedirs(link_to_test_folder+'/core_and_meta_all_runs')
-if not os.path.exists(link_to_test_folder+'/core_and_meta_all_runs/All_iterations'):
-    os.makedirs(link_to_test_folder+'/core_and_meta_all_runs/All_iterations')
-if not os.path.exists(link_to_test_folder+'/core_and_meta_all_runs/Best_iteration'):
-    os.makedirs(link_to_test_folder+'/core_and_meta_all_runs/Best_iteration')
+if not os.path.exists(path_save+'/core_and_meta_all_runs'):
+    os.makedirs(path_save+'/core_and_meta_all_runs')
+if not os.path.exists(path_save+'/core_and_meta_all_runs/All_iterations'):
+    os.makedirs(path_save+'/core_and_meta_all_runs/All_iterations')
+if not os.path.exists(path_save+'/core_and_meta_all_runs/Best_iteration'):
+    os.makedirs(path_save+'/core_and_meta_all_runs/Best_iteration')
 
 
 core_meta_dfs_archive = {'OTU':{'core':defaultdict(defaultdict), 'meta':defaultdict(defaultdict)}, 'SoFA': {'core':defaultdict(defaultdict), 'meta':defaultdict(defaultdict)}}
@@ -126,7 +202,7 @@ for profile in ['OTU','SoFA']:
         core_meta = {'core':[], 'meta':{'ID':[],'Count':[]}}
 
         for annot in np.unique(full_list):
-            if full_list.count(annot) == reps:
+            if full_list.count(annot) == int(args.nb_repeats):
                 core_meta['core'].append(annot)
             else:
                 core_meta['meta']['ID'].append(annot)
@@ -150,14 +226,14 @@ for profile in ['OTU','SoFA']:
         
 
 ##Getting the reference info for OTUs and annotations
-otu_db = pd.read_csv(link_to_test_folder+'/'+args.data_ref+'_1/Selection_outputs/run_1/OTU_'+dataset_name+'.csv')[['Average_importance','ID','Linked_Reactions','Family']]
-sofa_db = pd.read_csv(link_to_test_folder+'/'+args.data_ref+'_1/Selection_outputs/run_1/scores_'+dataset_name+'.csv')[['Average_importance','ID','Name','Linked_OTUs','Family']]
+otu_db = pd.read_csv(path_save+'/'+args.data_ref+'_1/Selection_outputs/run_1/OTU_'+dataset_name+'.csv')[['Average_importance','ID','Linked_Reactions','Family']]
+sofa_db = pd.read_csv(path_save+'/'+args.data_ref+'_1/Selection_outputs/run_1/scores_'+dataset_name+'.csv')[['Average_importance','ID','Name','Linked_OTUs','Family']]
 
 otu_db = otu_db[otu_db['ID']!='CUTOFF']
 sofa_db = sofa_db[sofa_db['ID']!='CUTOFF']
 
 ##Adding OTU taxonomies
-otu_names_ref = pd.read_csv(link_to_test_folder+'/'+args.data_ref+'_1/SoFAs/OTU_name_table_ref.tsv', sep='\t')
+otu_names_ref = pd.read_csv(path_save+'/'+args.data_ref+'_1/SoFAs/OTU_name_table_ref.tsv', sep='\t')
 otu_taxonomies = []
 
 for otu in otu_db['ID'].values:
@@ -189,6 +265,7 @@ for lab in labels:
 
 ##Formatting the Core and Meta output files
 for run in dict_lists[profile].keys():
+
     core_otu_df = core_meta_dfs_archive['OTU']['core'][run]
     meta_otu_df = core_meta_dfs_archive['OTU']['meta'][run]
     core_sofa_df = core_meta_dfs_archive['SoFA']['core'][run]
@@ -225,26 +302,20 @@ for run in dict_lists[profile].keys():
     core_otu_df_plus_info = core_otu_df_plus_info[['Average_importance','ID','Taxonomy','Linked_Reactions','Significant_linked_Reactions','Family']+['average score in '+str(lab) for lab in labels]]
     meta_otu_df_plus_info = meta_otu_df_plus_info[['ID','Count','Average_importance','Taxonomy','Linked_Reactions','Significant_linked_Reactions','Family']+['average score in '+str(lab) for lab in labels]].sort_values(by='Count', ascending=False)
 
-    core_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/core_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
-    meta_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/meta_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
-    core_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/core_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
-    meta_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/All_iterations/meta_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    core_sofa_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/All_iterations/core_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    meta_sofa_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/All_iterations/meta_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    core_otu_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/All_iterations/core_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    meta_otu_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/All_iterations/meta_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    
+    if run == 'run_'+str(max_ind_sofas+1):
+        core_sofa_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/Best_iteration/core_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+        meta_sofa_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/Best_iteration/meta_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
 
-    if run == max_ind_sofas:
-        core_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/core_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
-        meta_sofa_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/meta_annots_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
-
-    if run == max_ind_otus:
-        core_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/core_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
-        meta_otu_df_plus_info.to_csv(link_to_test_folder +'/core_and_meta_all_runs/Best_iteration/meta_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+    if run == 'run_'+str(max_ind_otus+1):
+        core_otu_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/Best_iteration/core_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
+        meta_otu_df_plus_info.to_csv(path_save +'/core_and_meta_all_runs/Best_iteration/meta_OTU_'+dataset_name+'_iteration_'+run.split('_')[-1]+'.csv', index=0)
 
     core_meta_dfs_archive['OTU']['core'][run] = core_otu_df_plus_info
     core_meta_dfs_archive['OTU']['meta'][run] = meta_otu_df_plus_info
     core_meta_dfs_archive['SoFA']['core'][run] = core_sofa_df_plus_info
     core_meta_dfs_archive['SoFA']['meta'][run] = meta_sofa_df_plus_info
-
-    
-
-
-
-
