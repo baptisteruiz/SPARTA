c5919e8c04f1e834940fa7a0d8f7a1b171e7a3aa
Yann Le Cunff <yann.le-cunff@irisa.fr>
2024-07-20 15:47:09 +0200

    lost arguments in the merge process, testing done, it works now

--- SPARTA/classification.py
+++ SPARTA/classification.py
@@ -147,7 +147,8 @@ def run_sparta_classification(functional_profile_filepath, label_filepath, outpu
         run_bank_of_average_importances_annots, run_bank_of_average_importances_taxons = run_iterate(functional_profile_filepath, label_filepath, run_output_folder,
                                                                                                                                                           run_nb, nb_iterations, esmecata_input, functional_occurrence_filepath,
                                                                                                                                                           otu_abundance_filepath, reference_test_sets_filepath,
-                                                                                                                                                          classifiers, method, var_ranking_method, seed_rf=seed_rf[0], seed_split=seed_split[0], seed_valid=seed_valid[0])
+                                                                                                                                                          classifiers, method, var_ranking_method, seed_rf=seed_rf[0], seed_split=seed_split[0], seed_valid=seed_valid[0]
+                                                                                                                                                          ,selected_otus_filepath = selected_otus_filepath, selected_annots_filepath = selected_annots_filepath)
         bank_of_selections_annots = update_iteration_dict(bank_of_selections_annots, run_bank_of_selections_annots)
         bank_of_selections_taxons = update_iteration_dict(bank_of_selections_taxons, run_bank_of_selections_taxons)
         bank_of_performance_dfs_annots = update_iteration_dict(bank_of_performance_dfs_annots, run_bank_of_performance_dfs_annots)
